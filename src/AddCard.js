9b9394f4206df3374b891172255efb6af711f295
Ricky <ahujashaurya13@gmail.com>
2023-07-17 20:03:22 -0400

    adding new component

---
+++ src/AddCard.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import axios from './axios';
+
+const AddCard = () => {
+    return (
+        <div>
+            <h1>Hello!!!!</h1>
+        </div>
+    )
+}
